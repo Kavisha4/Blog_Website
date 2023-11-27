03c6765bb5cc2442092ec3aaf996e5077b54584b
Kavisha4 <kvsha2002@gmail.com>
2023-11-27 16:26:49 +0530

    Handling Fetch

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Bloglist from './BlogList';
-
+import useFetch from './useFetch'
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('mario');
-  const [isPending, setIsPending]=useState(true);
-  const [error, setError]=useState(null);
+  
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter(blog => blog.id !== id);
   //   setBlogs(newBlogs);
   // };
+  const [name, setName] = useState('mario');
 
-  useEffect(() => {
-   setTimeout(()=>{
-    fetch('http://localhost:8000/blogs')
-    
-    .then(res=>{
-      console.log(res);
-      if(!res.ok)
-      {
-        throw Error('could not fetch data for that resource');
-      }
-      return res.json();
-    })
-    .then((data)=>{
-      console.log(data);
-      setBlogs(data);
-      setIsPending(false);
-      setError(null);
-    })
-    .catch(err=>{
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
-    })
-   },1000)
-  }, []);
-
+  const {data: blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
 
 
   return (
