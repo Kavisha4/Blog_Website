aed26fc5894da9d53b6e720353b4e19def0bda1d
Kavisha4 <kvsha2002@gmail.com>
2023-11-27 15:43:01 +0530

    Handling Fetch

--- src/Home.js
+++ src/Home.js
@@ -5,26 +5,46 @@ const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState('mario');
-
+  const [isPending, setIsPending]=useState(true);
+  const [error, setError]=useState(null);
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter(blog => blog.id !== id);
   //   setBlogs(newBlogs);
   // };
 
   useEffect(() => {
+   setTimeout(()=>{
     fetch('http://localhost:8000/blogs')
+    
     .then(res=>{
+      console.log(res);
+      if(!res.ok)
+      {
+        throw Error('could not fetch data for that resource');
+      }
       return res.json();
     })
     .then((data)=>{
       console.log(data);
       setBlogs(data);
+      setIsPending(false);
+      setError(null);
     })
+    .catch(err=>{
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    })
+   },1000)
   }, []);
+
+
 
   return (
     <div className="home">
       {/* {blogs && <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />} */}
+      {error && <div> {error} </div>}
+      {isPending && <div> Loading..</div>}
       {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
 
       <button onClick={() => setName('luigi')}>Change name</button>
